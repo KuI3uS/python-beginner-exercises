c7c12a8dbbd4a4b15eb81c02b83d2df08bca1b71
KuI3uS <kuba.marcinkowski98@wp.pl>
2024-10-20 16:17:10 +0200

    dodano program sprawdzania parzystosci liczb

---
+++ python-beginner-exercises/2.check_even_odd.py
@@ -0,0 +1,8 @@
+
+#Podaj liczbę
+liczba = int(input("Podaj liczbe która jest parzysta :"))
+
+if liczba % 2 == 0:
+    print(f"liczba jest parzysta : {liczba}")
+else:
+    print(f"liczba jest nieparzysta : {liczba}")
