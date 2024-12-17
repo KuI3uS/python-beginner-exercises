fb31cc984c0e63794072e1add9f7bdd758fb0050
KuI3uS <kuba.marcinkowski98@wp.pl>
2024-12-17 10:24:30 +0100

    variables

---
+++ course_from_scratch/first_steps/0.1 variables.py
@@ -0,0 +1,10 @@
+name = input("What is your name ?")
+lastName = input("what is your last name ?")
+old = input("what old are you ?")
+
+print ("hello : " + name + "\nlast name : " + lastName + "\nyour old : " + old)
+
+if int(old) <= 17:
+    print ("you are a minor")
+else:
+    print("you are of legal age")
--- course_from_scratch/first_steps/variables.py
+++
@@ -1,9 +0,0 @@
-name = input("What is your name?")
-age = input("What is your age ?")
-
-print("hello " + name + "! " + " your age is: " + age)
-
-if int(age) <= 17:
-    print("You are a minor")
-else:
-    print("you are an adult")
