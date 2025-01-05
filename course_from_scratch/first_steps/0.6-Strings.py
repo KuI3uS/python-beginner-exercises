2ff748014eeffa6cc0feb135753ca0f69578058f
KuI3uS <kuba.marcinkowski98@wp.pl>
2025-01-05 16:01:14 +0100

    string_methods

---
+++ course_from_scratch/first_steps/0.6-Strings.py
@@ -0,0 +1,5 @@
+channel_name = 'squid game'
+
+test = f'bardzo lubie film {channel_name}'
+print(test)
+
---
+++ course_from_scratch/first_steps/0.7-strings-methods.py
@@ -0,0 +1,7 @@
+chanel_name = 'Jakub lubi jezdzić na nartrach'
+
+print(chanel_name.split())# każde słowo jest osobno
+print(chanel_name.upper())# wszystkie lityery są pisane z dużej
+print(chanel_name.capitalize())# zaczya sie od dużej litery
+print(chanel_name.startswith('J'))# to powoduje z jakiej litery sie zaczyna
+print(chanel_name.replace("Jakub", "Edward"))#zmienia tekst
