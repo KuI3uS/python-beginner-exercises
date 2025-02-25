a651cf7815118af9cb3e04848e0c5a539e97c59a
KuI3uS <kuba.marcinkowski98@wp.pl>
2025-02-25 23:37:22 +0100

    0.19

--- course_from_scratch/first_steps/0.19-functions.py
+++ course_from_scratch/first_steps/0.19-functions.py
@@ -1,5 +1,9 @@
-def hello(name, age):
-    print("hello " + name + " " + str(age) + " Lat")
+def hello(name, age, last_name=None):
+    print("hello " + name + "! and " + str(age) + " Lat")
+    if last_name is not None:
+        print("my last name is " + last_name)
 
 
-hello("Jakub",26)
+hello("Jakub",26, 'Marcinkowski')
+# hello("Piotr", 23)
+# hello("Marta", 20)
