a0864b20a8aaf21e52e34ead68460f37322821ea
KuI3uS <kuba.marcinkowski98@wp.pl>
2025-03-18 15:21:28 +0100

    task 14

--- course_from_scratch/first_steps/13-lists-and-sets/max_min_sum_list.py
+++ course_from_scratch/first_steps/13-lists-and-sets/max_min_sum_list.py
@@ -1,3 +1,14 @@
 # 14.Napisz funkcję, która przyjmuje listę liczb i zwraca: największą liczbę, najmniejszą liczbę oraz sumę liczb.
 
+def max_min_sum(lista):
+    return max(lista), min(lista), sum(lista)
+
+liczby = {1,2,3,4,5,6,7,44,66,7,788,884}
+
+
+result = max_min_sum(liczby)
+
+print('the largest number', result[0])
+print('smallest number', result[1])
+print('sum of all numbers', result[2])
 
