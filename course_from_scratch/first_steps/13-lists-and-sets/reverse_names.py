ec01880717b203b532bd60ea11eb565e864dace4
KuI3uS <kuba.marcinkowski98@wp.pl>
2025-03-18 17:08:35 +0100

    task 15

--- course_from_scratch/first_steps/13-lists-and-sets/reverse_names.py
+++ course_from_scratch/first_steps/13-lists-and-sets/reverse_names.py
@@ -1,3 +1,11 @@
 # 15.Napisz funkcję, która przyjmuje listę imion i wypisuje je w odwrotnej kolejności, używając pętli.
 
 
+def reverse_numes(names):
+    for name in list(names[::-1]):
+        print(name)
+
+
+imiona = ["Jakub", "Marta", "Daria", "Marcin"]
+
+reverse_numes(imiona)
