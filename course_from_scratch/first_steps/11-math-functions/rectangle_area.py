b846c2c1b34cc5dbeade822675cef6d89c81cf65
KuI3uS <kuba.marcinkowski98@wp.pl>
2025-03-19 10:42:43 +0100

    task 16

--- course_from_scratch/first_steps/11-math-functions/rectangle_area.py
+++ course_from_scratch/first_steps/11-math-functions/rectangle_area.py
@@ -1,2 +1,10 @@
 # 16.Napisz funkcję, która przyjmuje długość i szerokość prostokąta, a następnie zwraca jego pole.
 
+def rectangle_area(a,b):
+    return a * b
+
+wynik = rectangle_area(3,5)
+
+print('area of a rectangle :', wynik)
+
+
