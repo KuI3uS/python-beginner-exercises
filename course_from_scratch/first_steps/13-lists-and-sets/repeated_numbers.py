9094d5ab6cd113d8ebd54981048b8ef972f5f86d
KuI3uS <kuba.marcinkowski98@wp.pl>
2025-03-20 13:38:32 +0100

    taks 17

--- course_from_scratch/first_steps/13-lists-and-sets/repeated_numbers.py
+++ course_from_scratch/first_steps/13-lists-and-sets/repeated_numbers.py
@@ -1,3 +1,19 @@
-# 17.Napisz funkcję, która przyjmuje listę liczb od użytkownika, a następnie zwraca te liczby, które występują na liście więcej niż raz. Jak nazwać te 17 zadań na gicie ?
+# 17.Napisz funkcję, która przyjmuje listę liczb od użytkownika, a następnie zwraca te liczby, które występują na liście więcej niż raz.
 
+def repeated_numbers(lista):
+    licznik = {}
 
+    for i in lista:
+        if i in licznik:
+            licznik[i] += 1
+        else:
+            licznik[i] = 1
+    duplikat = [liczba for liczba, ilosc in licznik.items() if  ilosc > 1]
+
+    return duplikat
+
+wejscie = input('your numbers : ')
+lista_liczb = list(map(int, wejscie.split()))
+
+wynik = repeated_numbers(lista_liczb)
+print('your numbers : ', wynik)
